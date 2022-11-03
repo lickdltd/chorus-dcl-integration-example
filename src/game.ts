895dc4fd338838bbc95d2706f48c602026b81cd8
Gary Rutland <me@garyrutland.com>
2022-11-03 15:04:25 +0000

    Basic Chorus setup

--- src/game.ts
+++ src/game.ts
@@ -1,51 +1,54 @@
-/// --- Set up a system ---
+import { getUserData } from '@decentraland/Identity'
+import { signedFetch } from '@decentraland/SignedFetch'
+import * as utils from '@dcl/ecs-scene-utils'
 
-class RotatorSystem {
-  // this group will contain every entity that has a Transform component
-  group = engine.getComponentGroup(Transform)
+const stationURL = 'https://chorus.lickd.co'
+const stream = '/lickd/test.mp3'
 
-  update(dt: number) {
-    // iterate over the entities of the group
-    for (const entity of this.group.entities) {
-      // get the Transform component of the entity
-      const transform = entity.getComponent(Transform)
+void executeTask(async () => {
+  const me = await getUserData()
 
-      // mutate the rotation
-      transform.rotate(Vector3.Up(), dt * 10)
+  onEnterSceneObservable.add(async (player) => {
+    if (player.userId === me?.userId) {
+      await radioStart()
     }
+  })
+})
+
+async function radioStart() {
+  try {
+    const response = await signedFetch(stationURL + '/api/session/create/dcl', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ stream })
+    })
+
+    if (response.text) {
+      const json = await JSON.parse(response.text)
+
+      if (json.token) {
+        const streamSource = new Entity()
+        const audioStream = new AudioStream(stationURL + stream + '?token=' + json.token)
+
+        streamSource.addComponent(audioStream)
+        streamSource.addComponent(new utils.Interval(60000, async () => await heartbeat(json.token)))
+
+        engine.addEntity(streamSource)
+      }
+    }
+  } catch (e) {
+    log(e)
   }
 }
 
-// Add a new instance of the system to the engine
-engine.addSystem(new RotatorSystem())
-
-/// --- Spawner function ---
-
-function spawnCube(x: number, y: number, z: number) {
-  // create the entity
-  const cube = new Entity()
-
-  // add a transform to the entity
-  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
-
-  // add a shape to the entity
-  cube.addComponent(new BoxShape())
-
-  // add the entity to the engine
-  engine.addEntity(cube)
-
-  return cube
+async function heartbeat(token: string) {
+  try {
+    await signedFetch(stationURL + '/api/session/heartbeat/dcl', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({ stream, token })
+    })
+  } catch (e) {
+    log(e)
+  }
 }
-
-/// --- Spawn a cube ---
-
-const cube = spawnCube(8, 1, 8)
-
-cube.addComponent(
-  new OnPointerDown(() => {
-    cube.getComponent(Transform).scale.z *= 1.1
-    cube.getComponent(Transform).scale.x *= 0.9
-
-    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
-  })
-)
