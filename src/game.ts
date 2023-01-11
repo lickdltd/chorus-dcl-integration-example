569d0ccc7445e386c13658abdb22d5c1290d9802
Gary Rutland <me@garyrutland.com>
2023-01-11 14:31:10 +0000

    Updated to use library

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,22 @@
-import { Chorus } from './chorus'
 import { getUserData } from '@decentraland/Identity'
+import * as chorus from '@lickdltd/chorus-dcl'
 
 void executeTask(async () => {
   const chorusEntity = new Entity()
-  const chorus = new Chorus(chorusEntity, '/genre/chill.mp3', 10000)
+  const chorusPlayer = new chorus.Player(chorusEntity, '/lickd/test.mp3')
   const me = await getUserData()
+
+  await chorusPlayer.start()
 
   onEnterSceneObservable.add(async (player) => {
     if (player.userId === me?.userId) {
-      await chorus.start()
+      await chorusPlayer.start()
     }
   })
 
   onLeaveSceneObservable.add(async (player) => {
     if (player.userId === me?.userId) {
-      await chorus.stop()
+      await chorusPlayer.stop()
     }
   })
 
